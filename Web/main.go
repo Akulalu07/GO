c2d6dd412ae845dd58fefb9e045f12fbcb134c07
Luka <puhlyakluka@yandex.ru>
2024-07-02 11:05:03 +0300

    ь
    Ь|

--- Web/main.go
+++ Web/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 )
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Hello!!!"))
+const form = `
+<html>
+    <head>
+    <title></title>
+    </head>
+    <body>
+        <form action="/" method="post">
+            <label>Логин</label><input type="text" name="login">
+            <label>Пароль<input type="password" name="password">
+            <input type="submit" value="Login">
+        </form>
+        <h1>Приветствую тебя на стартовой странице этого сайта!</h1>
+        <img src="/cat.jpg"" alt="это кот">
+    </body>
+</html>
+
+`
+
+func Auth(login, password string) bool {
+	return login == `guest` && password == `demo`
 }
-func apiPage(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Api Page!"))
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		login := r.FormValue("login")
+		password := r.FormValue("password")
+		if Auth(login, password) {
+			io.WriteString(w, "Добро пожаловать!")
+		} else {
+			http.Error(w, "Неверный логин или пароль", http.StatusUnauthorized)
+		}
+		return
+	} else {
+		io.WriteString(w, form)
+	}
 }
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/", apiPage)
-	mux.HandleFunc("/", mainPage)
-	err := http.ListenAndServe(":8080", mux)
-	fmt.Println("Hello")
+	err := http.ListenAndServe(`:8080`, http.HandlerFunc(mainPage))
 	if err != nil {
 		panic(err)
 	}
